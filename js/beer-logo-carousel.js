a0cd47b6e35176ff8ebd58f2bf84073aeca43497
Andrii Synytsia <andreysinitsa4315@gmail.com>
2024-02-28 19:51:06 -0600

    Added script JS for 'beer-logo-carousel'

--- js/beer-logo-carousel.js
+++ js/beer-logo-carousel.js
@@ -1,75 +1,38 @@
-const carousel = document.querySelector(".carousel__container");
-const slideContainer = document.querySelector(".carousel-slide__container");
-const prevBtn = document.querySelector(".carousel__button-prev");
-const nextBtn = document.querySelector(".carousel__button-next");
+const slides = document.querySelectorAll('.slide');
 
-let slidesPerView = getSlidesPerView();
-let slides = Array.from(slideContainer.children);
-let currentIndex = slidesPerView;
-
-function getSlidesPerView() {
-  if (window.innerWidth >= 1024) {
-    return 3;
-  }
-  if (window.innerWidth >= 768) {
-    return 2;
-  }
-  return 1;
-}
-
-function setupCarousel() {
-  slides = slides.filter((slide) => !slide.classList.contains("clone"));
-
-  const clonesStart = slides.slice(-slidesPerView).map(cloneSlide);
-  const clonesEnd = slides.slice(0, slidesPerView).map(cloneSlide);
-
-  slideContainer.append(...clonesStart, ...slides, ...clonesEnd);
-
-  updateCarousel();
-}
-
-function cloneSlide(slide) {
-  const clone = slide.cloneNode(true);
-  clone.classList.add("clone");
-  return clone;
-}
-
-function updateCarousel() {
-  slideContainer.style.transform = `translateX(-${
-    (currentIndex * 100) / slidesPerView
-  }%)`;
-}
-
-prevBtn.addEventListener("click", () => {
-  if (--currentIndex < 0) {
-    currentIndex = slides.length - slidesPerView * 2 - 1;
-    slideContainer.style.transition = "none";
-    updateCarousel();
-    requestAnimationFrame(() => {
-      requestAnimationFrame(() => {
-        slideContainer.style.transition = "";
-      });
-    });
-  }
-  updateCarousel();
+slides.forEach((slide, index) => {
+  slide.style.transform = `translateX(${index * 100}%)`;
 });
 
-nextBtn.addEventListener("click", () => {
-  slideContainer.style.transition = "";
-  if (++currentIndex >= slides.length - slidesPerView) {
-    currentIndex = slidesPerView;
-    slideContainer.style.transition = "none";
-    updateCarousel();
-    requestAnimationFrame(() => {
-      requestAnimationFrame(() => {
-        slideContainer.style.transition = "";
-      });
-    });
+let currentSlide = 0;
+
+let maxSlide = slides.length -1;
+
+const nextSlide = document.querySelector('.button-next');
+
+nextSlide.addEventListener('click', () => {
+  if (currentSlide === maxSlide) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
   }
-  updateCarousel();
+
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+  });
 });
 
-window.addEventListener("resize", () => {
-  slidesPerView = getSlidesPerView();
-  setupCarousel();
+const prevSlide = document.querySelector('.button-prev');
+
+prevSlide.addEventListener('click', () => {
+  if (currentSlide ===  0) {
+    currentSlide = maxSlide;
+  } else {
+    currentSlide--;
+  }
+
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+  });
 });
+
